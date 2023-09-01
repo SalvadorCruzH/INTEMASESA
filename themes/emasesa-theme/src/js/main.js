df3a64f7ba0f08045988cbbaa0ed43b2e9ba7427
Sara Saiz Santos <sssantos@minsait.com>
2023-09-01 14:32:07 +0200

    js main ready

--- themes/emasesa-theme/src/js/main.js
+++ themes/emasesa-theme/src/js/main.js
@@ -1,32 +1,15 @@
 
-/*
- * This function gets loaded when all the HTML, not including the portlets, is
- * loaded.
- */
-AUI().ready(function () {
+$(function() {
     menuFuntion.init();
 	menuDesktop.init();
 
 });
 
-/*
- * This function gets loaded after each and every portlet on the page.
- *
- * portletId: the current portlet's id
- * node: the Alloy Node object of the current portlet
- */
-Liferay.Portlet.ready(function (_portletId, _node) {});
-
-/*
- * This function gets loaded when everything, including the portlets, is on
- * the page.
- */
-Liferay.on('allPortletsReady', function () {});
-
 
  /**
  * Menu mobile
  */
+
  var menuFuntion = (function () {
 
 	var _toggleBtn = document.querySelector("#i-menuButton");
