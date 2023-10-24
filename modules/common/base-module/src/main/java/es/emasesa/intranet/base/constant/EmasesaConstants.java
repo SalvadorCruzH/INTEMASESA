cb5705cbd68cdbb354f75e16d726c0e4b62859c8
Moron <jamoron@minsait.com>
2023-10-24 12:38:20 +0200

    creacion del PDF y añadir a documentLibrary

--- modules/common/base-module/src/main/java/es/emasesa/intranet/base/constant/EmasesaConstants.java
+++ modules/common/base-module/src/main/java/es/emasesa/intranet/base/constant/EmasesaConstants.java
@@ -20,4 +20,6 @@ public class EmasesaConstants {
     public static final String EMASESA_EXPANDO_NOMBRE = "nombre";
 
     public static final String EMASESA_EXPANDO_USUARIO = "usuario";
+    public static final String EMASESA_FOLDER_COMPATIBILITY = "PDF Compatibilidad";
+    public static final String EMASESA_MIMETYPE = "application/pdf";
 }
