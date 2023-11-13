3b3e8cb1f6ead1fea9a7a15f7f3de7420ec4e589
Moron <jamoron@minsait.com>
2023-11-13 16:48:34 +0100

    Fragment obtener fecha actual

---
+++ themes/emasesa-fragments/src/emasesa/EM-Obtener fecha actual/index.js
@@ -0,0 +1,15 @@
+
+const inputElement = document.getElementById(`${fragmentNamespace}-text-input`);
+
+function main() {
+
+  var dateNow = new Date();
+
+  var day = dateNow.getDate();
+  var month = dateNow.getMonth() + 1;
+  var year = dateNow.getFullYear();
+  var dateFormatted = day + '/' + month + '/' + year;
+  inputElement.value = dateFormatted;
+}
+
+main();
