a78e7f95b0198cacb8dc41a00a9f67110b253dc0
INDRA\mcgimeno <mcgimeno@minsait.com>
2023-12-05 17:46:24 +0100

    Objecto Domiciliacion bancaria con validación de IBAN

---
+++ resources/objects/validaciones/validarIBAN.groovy
@@ -0,0 +1,25 @@
+import com.liferay.portal.template.*;
+import com.liferay.portal.util.*;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.util.tracker.ServiceTracker;
+import es.emasesa.intranet.base.util.CustomObjectUtil;
+
+Bundle bundleObject = FrameworkUtil.getBundle(CustomObjectUtil.class);
+ServiceTracker customObjectUtilTracker = new ServiceTracker(bundleObject.getBundleContext(),CustomObjectUtil.class, null);
+customObjectUtilTracker.open();
+CustomObjectUtil customObjectUtil = customObjectUtilTracker.getService();
+
+//boolean isValidIBAN = customObjectUtil.(new CustomObjectUtil().validarIBAN(iBAN));
+
+boolean isValidIBAN = customObjectUtil.validarIBAN(iBAN);
+
+if(isValidIBAN){
+	invalidFields = false;
+	return invalidFields;
+}else{
+	invalidFields = true;
+	return invalidFields;
+}
+
+customObjectUtilTracker.close();
