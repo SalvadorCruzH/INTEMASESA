eed6ea39c9225950d74bd3a1466c257cb918729d
INDRA\mcgimeno <mcgimeno@minsait.com>
2023-12-05 17:03:02 +0100

    Se añade clase para validaciones del Object a través de groovy. Se añade validacion de IBAN

---
+++ modules/common/base-module/src/main/java/es/emasesa/intranet/base/util/CustomObjectUtil.java
@@ -0,0 +1,48 @@
+package es.emasesa.intranet.base.util;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import java.math.BigInteger;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component(
+        immediate = true,
+        service = CustomObjectUtil.class
+)
+public class CustomObjectUtil {
+	
+	 public static boolean validarIBAN(String iban) {
+	        LoggerUtil.debug(LOG,"Validando el IBAN: " + iban);
+	        
+	        LoggerUtil.debug(LOG,"Paso 1: Verificar el formato del IBAN con una expresión regular");
+	        String regex = "^[A-Z]{2}\\d{22}$";
+	        if (!iban.matches(regex)) {
+	        	LoggerUtil.debug(LOG,"El formato del IBAN es incorrecto");
+	            return false;
+	        }else {
+	        	LoggerUtil.debug(LOG,"El formato del IBAN es correcto, se sigue con la validación");
+	        }
+
+	        LoggerUtil.debug(LOG,"Paso 2: Realizar la verificación del dígito de control");
+	        String letrasIban = iban.substring(0, 2);
+	        String digitosIban = iban.substring(4) + letrasIban + iban.substring(2, 4);
+
+	        LoggerUtil.debug(LOG,"Reemplazar letras por sus equivalentes numéricos");
+	        StringBuilder ibanNumerico = new StringBuilder();
+	        for (int i = 0; i < digitosIban.length(); i++) {
+	            char caracter = digitosIban.charAt(i);
+	            if (Character.isLetter(caracter)) {
+	                ibanNumerico.append(Character.getNumericValue(caracter));
+	            } else {
+	                ibanNumerico.append(caracter);
+	            }
+	        }
+
+	        BigInteger ibanNumericoBigInt = new BigInteger(ibanNumerico.toString());
+	        return ibanNumericoBigInt.mod(BigInteger.valueOf(97)).intValue() == 1;
+	    }
+	 
+	 private static final Log LOG = LogFactoryUtil.getLog(CustomObjectUtil.class);
+}
