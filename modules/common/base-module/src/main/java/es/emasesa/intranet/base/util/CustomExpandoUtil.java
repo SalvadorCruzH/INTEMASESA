88bedc06185f0fead3b00f0e8b8748a756cf244b
INDRA\mcgimeno <mcgimeno@minsait.com>
2023-12-14 11:54:34 +0100

    Se añaden métodos para obtener usuarios por valor de campo expando

--- modules/common/base-module/src/main/java/es/emasesa/intranet/base/util/CustomExpandoUtil.java
+++ modules/common/base-module/src/main/java/es/emasesa/intranet/base/util/CustomExpandoUtil.java
@@ -4,13 +4,22 @@ import com.liferay.expando.kernel.exception.DuplicateColumnNameException;
 import com.liferay.expando.kernel.exception.NoSuchTableException;
 import com.liferay.expando.kernel.model.ExpandoColumn;
 import com.liferay.expando.kernel.model.ExpandoTable;
+import com.liferay.expando.kernel.model.ExpandoTableConstants;
+import com.liferay.expando.kernel.model.ExpandoValue;
 import com.liferay.expando.kernel.service.ExpandoColumnLocalService;
 import com.liferay.expando.kernel.service.ExpandoColumnLocalServiceUtil;
 import com.liferay.expando.kernel.service.ExpandoTableLocalService;
 import com.liferay.expando.kernel.service.ExpandoTableLocalServiceUtil;
+import com.liferay.expando.kernel.service.ExpandoValueLocalService;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 import com.liferay.portal.kernel.util.Validator;
+
+import java.util.List;
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -26,6 +35,9 @@ public class CustomExpandoUtil {
 
     @Reference
     ExpandoColumnLocalService _expandoColumnLocalService;
+    
+    @Reference
+    private ExpandoValueLocalService _expandoValueLocalService;
 
     public boolean createExpando(final long companyId,
                                   final String tableClassName,
@@ -93,6 +105,90 @@ public class CustomExpandoUtil {
             column = ExpandoColumnLocalServiceUtil.getColumn(tableId, columnName);
         }
         return column;
+    }
+    
+    /**
+     * Obtener el classPk de un expando por su valor
+     * @param expandoTable
+     * @param columnName
+     * @param columnValue
+     * 
+     * @return classPK
+     */
+    public long getClassPKByExpandoValue(long companyId, String columnName, String columnValue) {
+    	long classPK = 0;
+    	
+    	 try {
+    		 _log.debug("Buscando expando values para la columna: " + columnName);
+	    	ExpandoTable expandoTable = _expandoTableLocalService.getTable(companyId, User.class.getName(), ExpandoTableConstants.DEFAULT_TABLE_NAME);
+	    	_log.debug("Obtenido el expando table: " + expandoTable.getTableId());
+	        List<ExpandoValue> expandoValues = _expandoValueLocalService.getColumnValues(
+	            expandoTable.getCompanyId(), User.class.getName(), ExpandoTableConstants.DEFAULT_TABLE_NAME, columnName, -1, -1);
+	        
+	        if(Validator.isNotNull(expandoValues) && expandoValues.size() > 0) {
+	        	_log.debug("Encontrado valores para el expando: " + columnName);
+	        	 for (ExpandoValue expandoValue : expandoValues) {
+	                 if (columnValue.equals(expandoValue.getData())) {
+	                	 _log.debug("Encontrado un expando con el valor: " + columnValue);
+	                 	classPK = expandoValue.getClassPK();
+	                 	_log.debug("Se obtiene el classPK: " + classPK);
+	                 	return classPK;
+	                 }
+	             }
+	        }else {
+	        	_log.debug("No se han obtenido valores para el expando: " + columnName);
+	        }
+         } catch (PortalException e) {
+        	 _log.error("La tabla de expando no existe", e);
+		}   
+        return classPK;
+    }
+    
+    /**
+     * Recupera un usuario por su valor
+     * @param companyId
+     * @param expandoName
+     * @param expandoValue
+     * 
+     * @return user
+     */
+    public User getUserByExpandoValue(long companyId, String expandoName, String expandoValue) {
+        User user = null;
+        try {
+        	_log.debug("Buscando usuario por expando valor: " + expandoValue );
+	        long userId = getClassPKByExpandoValue(companyId, expandoName, expandoValue);
+	        if(Validator.isNotNull(userId)) {
+	        	_log.debug("Obtenido el userId: " + userId);
+		        user = UserLocalServiceUtil.getUser(userId);
+		        _log.debug("Encontrado usuario con el valor: " + expandoValue + ". El usuario es: " + user.getScreenName());
+	        }else {
+	        	_log.debug("No se ha encontrado ningún usuario con el valor: " + expandoValue);
+	        }
+        } catch (PortalException e) {
+        	_log.error("Error al obtener el usuario", e);
+		}
+        return user;
+    }
+    
+    
+    /**
+     * Recupera el valor de un campo expando dado un usuario
+     * @param userId
+     * @param companyId
+     * @param expandoName
+     * 
+     * @return expandoValue
+     */
+    public String getDataValueByUser (long userId, long companyId, String expandoName) {
+    	String expandoValue = StringPool.BLANK;
+    	 try {
+    		 _log.debug("Obteniendo el valor del expando: " + expandoName + " para el usuario: " +  userId);
+    		 expandoValue = _expandoValueLocalService.getData(companyId, User.class.getName(), ExpandoTableConstants.DEFAULT_TABLE_NAME, expandoName, userId, StringPool.BLANK);
+    		 _log.debug("El valor obtenido es: " + expandoValue);
+    	 } catch (PortalException e) {
+    		 _log.error("Se ha producido un error al intentar obtener el valor del expando: " + expandoName);
+    	 }
+    	return expandoValue;
     }
 
 
