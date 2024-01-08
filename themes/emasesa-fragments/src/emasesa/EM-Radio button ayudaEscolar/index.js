7cd6903f5d9b76e0c54639c47412a4ea38d4e0ff
Moron <jamoron@minsait.com>
2024-01-08 16:25:06 +0100

    modificado fragmento de ayuda escolar

--- themes/emasesa-fragments/src/emasesa/EM-Radio button ayudaEscolar/index.js
+++ themes/emasesa-fragments/src/emasesa/EM-Radio button ayudaEscolar/index.js
@@ -5,7 +5,6 @@ const familiaMonoparental = document.getElementById('familiaMonoparental');
 const familiaNumerosa = document.getElementById('familiaNumerosa');
 const familiaNumerosaInput = document.getElementsByName('familiaNumerosa')[0];
 const familiaMonoparentalInput = document.getElementsByName('familiaMonoparental')[0];
-
 var radioButtonsBeneficiarios = document.querySelectorAll('input[type="radio"][name="beneficiarios"]');
 var radioButtonsEstudios = document.querySelectorAll('input[type="radio"][name="estudios"]');
 
@@ -14,17 +13,10 @@ parentElement.addEventListener('click', function (event) {
     if (target.tagName != 'INPUT') return;
     valueInputElement.value = target.value;
     labelInputElement.value = target.closest('label').innerText.trim();
-    if (target.name == 'beneficiarios') {
-        radioButtonsBeneficiarios.forEach(function (radioButton) {
-            radioButton.checked = false;
-        });
-        target.checked = true;
-    } else if (target.name == 'estudios') {
-        radioButtonsEstudios.forEach(function (radioButton) {
-            radioButton.checked = false;
-        });
-        target.checked = true;
-    }
+
+  if (target.name == 'beneficiarios' || target.name == 'estudios') {
+    buttonSubmit();
+  }
     setearValores();
 });
 function setearValores() {
@@ -34,4 +26,31 @@ function setearValores() {
   $('[name = "familiaNumerosa"]').text($('#familiaNumerosa').val());
   $('[name = "numero"]').val($('[name="beneficiarios"]:checked').attr('data-number'));
   $('[name = "numero"]').text($('[name="beneficiarios"]:checked').attr('data-number'));
+}
+
+function buttonSubmit() {
+  var buttonSubmit = document.querySelector('[id^="fragment-"][id$="-submit-button"]');
+  var ayudasSolicitadas = document.getElementById('ayudasSolicitadas');
+
+  if (document.querySelector('input[name="beneficiarios"]:checked') !== null) {
+    var selectedBeneficiarios = document.querySelector('input[name="beneficiarios"]:checked');
+    var labelInputElementBeneficiarios = selectedBeneficiarios.closest('label');
+    var labelTextBeneficiarios = labelInputElementBeneficiarios.innerText.split('-')[0];
+  }
+
+  if (document.querySelector('input[name="estudios"]:checked') !== null){
+    var selectedEstudios = document.querySelector('input[name="estudios"]:checked');
+    var labelInputElementEstudios = selectedEstudios.closest('label');
+    var labelTextEstudios = labelInputElementEstudios.innerText.split('-')[1];
+  }
+
+  if ((labelTextEstudios != null || labelTextEstudios !== undefined) && (labelTextBeneficiarios != null || labelTextBeneficiarios !== undefined)){
+    var combinados = labelTextBeneficiarios.trim() + '-' + labelTextEstudios.trim();
+  }
+
+  if (ayudasSolicitadas && ayudasSolicitadas.innerText.includes(combinados)) {
+    buttonSubmit.disabled = true;
+  } else {
+    buttonSubmit.disabled = false;
+  }
 }
