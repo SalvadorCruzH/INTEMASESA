6a2cf9249bb8ae6fdaf80171213aad6a825975ab
Ignacio T <itriguero@minsait.com>
2024-01-19 13:58:27 +0100

    Convertido nombre sin espacio a con espacio en mis gestiones

--- modules/custom/ajaxsearch/emasesa-ajaxsearch/src/main/java/es/emasesa/intranet/portlet/ajaxsearch/impl/solicitudes/result/ConsultaFormacionResultImpl.java
+++ modules/custom/ajaxsearch/emasesa-ajaxsearch/src/main/java/es/emasesa/intranet/portlet/ajaxsearch/impl/solicitudes/result/ConsultaFormacionResultImpl.java
@@ -201,7 +201,6 @@ public class ConsultaFormacionResultImpl implements AjaxSearchResult {
     private JSONObject getResultJson(final Document document,
                                      final ThemeDisplay themeDisplay) {
         final JSONObject jsonObject = jsonFactory.createJSONObject();
-        jsonObject.put(AjaxSearchPortletKeys.NOMBRE_OBJETO, document.get(themeDisplay.getLocale(), AjaxSearchPortletKeys.OBJECT_DEFINITION_NAME));
 
         String fechaSolicitud = document.get(themeDisplay.getLocale(), Field.CREATE_DATE);
         jsonObject.put(AjaxSearchPortletKeys.FECHA_SOLICITUD, ajaxSearchUtil.formatDate(fechaSolicitud));
@@ -265,6 +264,7 @@ public class ConsultaFormacionResultImpl implements AjaxSearchResult {
             try {
                 String objectDefinitionName = document.get(themeDisplay.getLocale(), AjaxSearchPortletKeys.OBJECT_DEFINITION_NAME);
                 String objectName = JSONFactoryUtil.createJSONObject(clientExtensionsSettings.objectNames()).getString(objectDefinitionName, objectDefinitionName);
+                jsonObject.put(AjaxSearchPortletKeys.NOMBRE_OBJETO, objectName);
                 String json = clientExtensionsSettings.objectMapping();
                 JSONObject jsonObject1 = JSONFactoryUtil.createJSONObject(json);
                 String stringObject = jsonObject1.getString(objectName);
