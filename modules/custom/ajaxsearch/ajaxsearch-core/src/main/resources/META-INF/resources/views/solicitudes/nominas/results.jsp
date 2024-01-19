86767b2fed6b7929eb6931ac2cbe9f379b7da3e0
Salvador Cruz <scruzh@minsait.com>
2024-01-19 11:52:36 +0100

    INTEMASESA-34 Nominas subida de codigo parcial

---
+++ modules/custom/ajaxsearch/ajaxsearch-core/src/main/resources/META-INF/resources/views/solicitudes/nominas/results.jsp
@@ -0,0 +1,158 @@
+<%@ include file="init.jsp" %>
+
+<div class="m-searchAjax m-searchAjax--results">
+    <div>
+        <div id="as-total-items">
+            <!-- EMPTY BY DEFAULT -->
+        </div>
+
+        <div class="m-results-wrapper ema-documentos-ajax ema-ajaxsearch">
+            <div class="ema-table__wrapper">
+                <table class="ema-table__ajaxsearch">
+                    <thead>
+                     <tr>
+                      <th><liferay-ui:message key="es.emasesa.intranet.nominas.periodo" /></th>
+                      <th><liferay-ui:message key="es.emasesa.intranet.nominas.provisional" /></th>
+                      <th><liferay-ui:message key="es.emasesa.intranet.nominas.definitiva" /></th>
+                      <th><liferay-ui:message key="es.emasesa.intranet.nominas.ultimoRecalculo" /></th>
+                      <th><liferay-ui:message key="es.emasesa.intranet.nominas.fechaRecalculo" /></th>
+                     </tr>
+                    </thead>
+                    <tbody id="as-wrapper">
+                        <!-- -->
+                    </tbody>
+                </table>
+            </div>
+            <div id="wrapper-not-result" class="d-none">
+                <liferay-ui:message key="no-results" />
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<template id="as-total-items-template" class="d-none">
+    <div class="m-ajaxresults-header-element">
+        <liferay-ui:message key="es.emasesa.intranet.ajaxsearch.found-sing" /> #total-items# <liferay-ui:message key="es.emasesa.intranet.ajaxsearch.document" />
+    </div>
+
+    <div class="m-ajaxresults-header-elements">
+        <liferay-ui:message key="es.emasesa.intranet.ajaxsearch.found-plur" /> #total-items# <liferay-ui:message key="es.emasesa.intranet.ajaxsearch.documents" />
+    </div>
+
+    <div class="d-none"> #items-page# documentos por página</div>
+</template>
+
+
+<template id="as-template">
+    <tr>
+        <td>#periodo#</td>
+				<td>#nominaProvisional#</td>
+            <c:if test="${exResult eq '2'}">
+                <td class="ema-td-dropdown">
+                    <a href="#urlVisualizar#" class="ema-enlace-visualizar"><i class="fa-solid fa-eye"></i></a>
+                    <!-- Boton de descarga -->
+                    <a href="javascript:void(0);" class="ema-boton-descargar" onclick="descargarPDF('#valor#', '"#pdf#"')">
+                        <i class="fa-solid fa-download"></i> Descargar
+                    </a>
+                </td>
+            </c:if>
+    </tr>
+</template>
+
+<script>
+    // Función para descargar el PDF
+    function descargarPDF(pdfBase64, nombreArchivo) {
+        // Convierte el base64 a Blob
+        var byteCharacters = atob(pdfBase64);
+        var byteNumbers = new Array(byteCharacters.length);
+        for (var i = 0; i < byteCharacters.length; i++) {
+            byteNumbers[i] = byteCharacters.charCodeAt(i);
+        }
+        var byteArray = new Uint8Array(byteNumbers);
+        var blob = new Blob([byteArray], { type: 'application/pdf' });
+
+        // Crea un enlace temporal y lo simula haciendo clic
+        var link = document.createElement('a');
+        link.href = window.URL.createObjectURL(blob);
+        link.download = nombreArchivo; // Nombre del archivo
+        link.click();
+    }
+</script>
+
+<script>
+ajaxSearchGlobalConfig = {
+    _preAppendItem : function (newItem, jsonItem) {return newItem},
+    _postdrawItem : function (jsonItem) {},
+    _predrawAll : function (payload) {},
+    _postdrawAll : function (payload) {
+        addClickFunctionality();
+        checkStatus();
+    }
+}
+$(document).ready(function () {
+    var options = $(".results-pagination-select-container .results-pagination-select option");
+    for (var i = 0; i < options.length; i++) {
+        var urlParams = new URLSearchParams(window.location.search);
+        var curPage = urlParams.get('currentPage');
+        if (curPage == null) {
+            curPage = 1;
+        }
+        var value = options[i].value;
+        if (value == curPage) {
+            options[i].selected = true;
+        }
+    }
+    $(document).mouseup(function(e) {
+        var container = $(".ema-desplegable-moreoptions");
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            container.removeClass('show');
+        }
+    });
+
+
+});
+
+var addClickFunctionality = function () {
+    var moreOptionsTd = $('.ema-td-dropdown');
+    moreOptionsTd.each(function() {
+        $(this).children('.ema-button-moreoptions').on('click', function () {
+            $(this).siblings('.ema-desplegable-moreoptions').toggleClass('show');
+        });
+    });
+}
+
+var checkStatus = function () {
+    $('tbody#as-wrapper tr').each(function() {
+        var estado = $(this).find('.ema-pill-estado');
+        if(estado.hasClass("success")) {
+            $(this).find(".ema-button-moreoptions").remove();
+            $(this).find(".ema-desplegable-moreoptions").remove();
+        } else if (estado.hasClass("danger") ) {
+            $(this).find(".ema-button-moreoptions").remove();
+            $(this).find(".ema-desplegable-moreoptions").remove();
+        } else {
+            $(this).find('.ema-enlace-visualizar').remove();
+        }
+    });
+}
+
+
+var openEditDialog = function (url) {
+    Liferay.Util.openWindow({
+        dialog: {
+            destroyOnHide: true,
+            modal: true,
+            after: {
+                render: function(event) {
+                    //
+                }
+            }
+        },
+        id: 'EditInfoIntDialog',
+        refreshWindow: window,
+        title: 'Consultar',
+        uri: url
+    });
+}
+</script>
---
+++ modules/custom/ajaxsearch/emasesa-ajaxsearch/src/main/java/es/emasesa/intranet/portlet/ajaxsearch/impl/solicitudes/result/ConsultaNominasResultImpl.java
@@ -0,0 +1,264 @@
+package es.emasesa.intranet.portlet.ajaxsearch.impl.solicitudes.result;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.liferay.expando.kernel.model.ExpandoTableConstants;
+import com.liferay.expando.kernel.service.ExpandoValueLocalService;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.ParseException;
+import com.liferay.portal.kernel.search.SearchException;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+import es.emasesa.intranet.base.constant.StringConstants;
+import es.emasesa.intranet.base.model.AjaxMessage;
+import es.emasesa.intranet.base.util.CustomCacheSingleUtil;
+import es.emasesa.intranet.base.util.CustomDateUtil;
+import es.emasesa.intranet.base.util.LoggerUtil;
+import es.emasesa.intranet.portlet.ajaxsearch.base.AjaxSearchDisplayContext;
+import es.emasesa.intranet.portlet.ajaxsearch.constant.AjaxSearchPortletKeys;
+import es.emasesa.intranet.portlet.ajaxsearch.model.AjaxSearchJsonModel;
+import es.emasesa.intranet.portlet.ajaxsearch.model.AjaxSearchResult;
+import es.emasesa.intranet.portlet.ajaxsearch.util.AjaxSearchUtil;
+import es.emasesa.intranet.service.util.SapServicesUtil;
+import es.emasesa.intranet.settings.osgi.RolesSettings;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import es.emasesa.intranet.sigd.service.application.SigdServiceApplication;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+import java.time.Year;
+import java.util.*;
+
+@Component(
+        immediate = true,
+        property = { },
+        service = AjaxSearchResult.class
+)
+public class ConsultaNominasResultImpl implements AjaxSearchResult {
+
+	private final static Log LOG = LoggerUtil.getLog(ConsultaNominasResultImpl.class);
+
+	private static final Properties DFLT_PROPERTIES = new Properties();
+
+	public static final String TEMPLATE_KEY = "template-key";
+	public static final String STRUCTURE_KEY = "structure-key";
+	public static final String CSS_WRAPPER_CLASS = "css-wrapper-class";
+	public static final String DISABLE_PAGINATION = "disable-pagination";
+	private static final String CAT_SELECTED = "catSelected";
+	private static final String FECHA_DESDE = "fechaDesde";
+	private static final String FECHA_HASTA = "fechaHasta";
+
+
+
+	static {
+		DFLT_PROPERTIES.put(AjaxSearchPortletKeys.PROP_BASE_PAGESIZE,
+							AjaxSearchPortletKeys.PROP_BASE_PAGESIZE_DFLT);
+
+		DFLT_PROPERTIES.put(TEMPLATE_KEY, "EMA-NOTICIA-TARJETA-TEMPLATE");
+		DFLT_PROPERTIES.put(STRUCTURE_KEY, "EMA-NOTICIA");
+		DFLT_PROPERTIES.put(CSS_WRAPPER_CLASS, "");
+		DFLT_PROPERTIES.put(DISABLE_PAGINATION, "0");
+	}
+
+	@Override
+	public Properties getDefaultProperties() {
+		return DFLT_PROPERTIES;
+	}
+
+	@Override
+	public AjaxMessage filterResults(PortletRequest request,
+									 PortletResponse response,
+									 AjaxSearchDisplayContext ajaxSearchDisplayContext) {
+
+		try {
+			long totalItems = 0;
+			final int currentPage = ajaxSearchDisplayContext.getCurrentPage();
+			final int pageSize = ajaxSearchDisplayContext.getPageSize();
+			final JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+			final String disablePaginationStr = ajaxSearchDisplayContext.getConfig().getOrDefault(DISABLE_PAGINATION, StringConstants.ZERO);
+			final boolean disablePagination = !Validator.isBlank(disablePaginationStr) && disablePaginationStr.equals("1");
+
+            Calendar calendar = Calendar.getInstance();
+            String year = calendar.get(Calendar.YEAR)+"";
+
+			if(year.isBlank()){
+				year = String.valueOf(Year.now().getValue());
+			}
+            totalItems = performSearchAndParse(request,
+                response,
+                ajaxSearchDisplayContext,
+                    year,
+                currentPage,
+                pageSize,
+                disablePagination,
+                jsonArray);
+
+			final int totalPages = (((int) totalItems + pageSize - 1) / pageSize);
+			final AjaxSearchJsonModel ajaxSearchJsonModel = new AjaxSearchJsonModel(
+					currentPage,
+					totalPages,
+					totalItems,
+					(disablePagination? 0: pageSize),
+					jsonArray);
+			return new AjaxMessage(
+					AjaxMessage.STATUS.OK,
+					AjaxMessage.MESSAGES_DEFAULT.OK,
+					ajaxSearchJsonModel.getJsonObject());
+
+		} catch (ParseException | SearchException e) {
+			LOG.info("Error At search in indexes: " + e.getLocalizedMessage());
+			final AjaxSearchJsonModel ajaxSearchJsonModel = new AjaxSearchJsonModel(0, 0, 0L, 0, JSONFactoryUtil.createJSONArray());
+			return new AjaxMessage(
+					AjaxMessage.STATUS.ERROR_GENERAL,
+					AjaxMessage.MESSAGES_DEFAULT.ERROR_GENERAL,
+					ajaxSearchJsonModel.getJsonObject());
+		}
+	}
+
+	private static final Date NULL_DATE=null;
+	private static final List<String> EMPTY_LIST=new ArrayList<>();
+
+	private long performSearchAndParse(final PortletRequest request,
+									   final PortletResponse response,
+									   final AjaxSearchDisplayContext ajaxSearchDisplayContext,
+									   final String year,
+									   final int currentPage,
+									   final int pageSize,
+									   final boolean disablePagination,
+									   final JSONArray jsonArray) throws ParseException, SearchException {
+
+		final ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
+		JSONArray array = null;
+		int totalItems = 0;
+		String matricula ="";
+
+		try {
+			matricula = _expandoValueLocalService.getData(
+					themeDisplay.getCompanyId(),
+					User.class.getName(),
+					ExpandoTableConstants.DEFAULT_TABLE_NAME,
+					"matricula",
+					themeDisplay.getUser().getUserId(),
+					StringPool.BLANK
+			);
+
+		} catch (Exception e) {
+			LoggerUtil.error(LOG, "ERROR getValue from Expando", e);
+		}
+
+		String listadoNominas = _sigdServiceApplication.buscarDocumento(matricula);
+		try {
+			JSONObject jsonObject = JSONFactoryUtil.createJSONObject(listadoNominas);
+			JSONObject buscarDocumentosResponse = jsonObject.getJSONObject("buscarDocumentosResponse");
+
+			if (buscarDocumentosResponse != null) {
+				JSONArray elementos = buscarDocumentosResponse.getJSONArray("elementos");
+
+				if (elementos != null) {
+					for (int i = 0; i < elementos.length(); i++) {
+						JSONObject elemento = elementos.getJSONObject(i);
+						JSONObject documentoOrigen = elemento.getJSONObject("documentoOrigen");
+
+						String codigoTipoDocumental = documentoOrigen.getString("codigoTipoDocumental");
+						if ("7972".equals(codigoTipoDocumental)) {
+							String urlNominaDefinitiva = documentoOrigen.getString("urlDescarga");
+						} else if ("7971".equals(codigoTipoDocumental)) {
+							String urlNominaProvisional = documentoOrigen.getString("urlDescarga");
+						} else if ("7973".equals(codigoTipoDocumental)) {
+							String urlUltimoRecalculo = documentoOrigen.getString("urlDescarga");
+						} else {
+							System.out.println("El valor no es ninguno de los esperados");
+						}
+						String urlDescarga = documentoOrigen.getString("urlDescarga");
+
+						JSONArray campos = documentoOrigen.getJSONArray("campos");
+						for (int b = 0; b < campos.length(); b++) {
+							JSONObject campo = campos.getJSONObject(b);
+							String nombreOrigen = campo.getString("nombreOrigen");
+							String dateValue = campo.getString("dateValue");
+						}
+					}
+				} else {
+					System.out.println("La matriz de elementos es nula.");
+				}
+			} else {
+				System.out.println("El objeto buscarDocumentosResponse es nulo.");
+
+			}
+		} catch (JSONException e) {
+			throw new RuntimeException(e);
+		}
+
+
+		if(Validator.isNotNull(year)){
+
+			String cacheKey = "certificadoRetenciones_"+year+themeDisplay.getUser().getScreenName();
+			Object object = _cache.get(cacheKey);
+
+			if(Validator.isNotNull(object) && ((JSONArray) object).length()>0){
+				array = (JSONArray) object;
+
+			}else{
+				int yearActual = Integer.parseInt(year);
+				array = JSONFactoryUtil.createJSONArray();
+				for (int i = 0; i < 5; i++) {
+					yearActual = yearActual - 1;
+
+					JSONObject x = _sapServicesUtil.getRetenciones(matricula, String.valueOf(yearActual));
+					array.put(x);
+				}
+				//_cache.put(cacheKey, array, 86400);
+			}
+
+			final int start = disablePagination ? 0 : ((currentPage - 1) * pageSize);
+			int count = (currentPage * pageSize) > array.length() ? array.length() : (currentPage * pageSize);
+			final int end = disablePagination ? pageSize : count;
+
+			totalItems = array.length();
+			List<JSONObject> listJson = new ArrayList<>();
+			for(int i = 0;i<array.length();i++){
+
+				listJson.add(array.getJSONObject(i));
+			}
+
+			listJson.subList(start,end).stream().forEach(j->{
+				jsonArray.put(j);
+			});
+		}
+		return totalItems;
+	}
+
+	private static final String VIEW = "/views/solicitudes/nominas/results.jsp";
+
+	@Override
+	public String getResultsView(PortletRequest request, PortletResponse response) {
+		final Map<String, String> config = ajaxSearchUtil.getPropertiesMap(request);
+		request.setAttribute("cssWrapperClass", config.getOrDefault(CSS_WRAPPER_CLASS, StringPool.BLANK));
+		request.setAttribute("disablePagination", config.getOrDefault(DISABLE_PAGINATION, StringConstants.ZERO));
+		return VIEW;
+	}
+
+	@Reference
+	AjaxSearchUtil ajaxSearchUtil;
+	@Reference
+	SapServicesUtil _sapServicesUtil;
+	@Reference
+	CustomDateUtil _customDateUtil;
+	@Reference
+	SigdServiceApplication _sigdServiceApplication;
+	@Reference
+	CustomCacheSingleUtil _cache;
+	@Reference
+	RolesSettings _rolesSettings;
+	@Reference
+	ExpandoValueLocalService _expandoValueLocalService;
+}
